5e7bcbead1f3601a45d9c3465f001718f59c01f6
Your Name <alexsanderrus@gmail.com>
2026-03-30 17:24:02 +0500

    module2-task1

--- src/mock/destinations.js
+++ src/mock/destinations.js
@@ -18,19 +18,19 @@ export const destinations = [
     id: 'dest-1',
     name: 'Amsterdam',
     description: 'Amsterdam is the Netherlands\' capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city\'s 17th-century Golden Age.',
-    photos: [] // Убрали фотографии, чтобы не было ошибок 404
+    photos: [] 
   },
   {
     id: 'dest-2',
     name: 'Geneva',
     description: 'Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.',
-    photos: [] // Убрали фотографии
+    photos: [] 
   },
   {
     id: 'dest-3',
     name: 'Chamonix',
     description: 'Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it\'s renowned for its skiing.',
-    photos: [] // Убрали фотографии
+    photos: [] 
   }
 ];
 
